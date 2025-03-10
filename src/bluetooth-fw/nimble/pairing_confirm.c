8491e88fe9494afeb24d12a6018718c39efa6b64
Liam McLoughlin <lmcloughlin@google.com>
2025-03-10 23:29:28 +0000

    bluetooth: implement most of the necessary GAP events

--- src/bluetooth-fw/nimble/pairing_confirm.c
+++ src/bluetooth-fw/nimble/pairing_confirm.c
@@ -15,5 +15,20 @@
  */
 
 #include <bluetooth/pairing_confirm.h>
+#include <stdint.h>
 
-void bt_driver_pairing_confirm(const PairingUserConfirmationCtx *ctx, bool is_confirmed) {}
+#include "host/ble_hs.h"
+#include "host/ble_sm.h"
+#include "system/logging.h"
+#include "system/passert.h"
+
+void bt_driver_pairing_confirm(const PairingUserConfirmationCtx *ctx, bool is_confirmed) {
+  uint16_t conn_handle = (uintptr_t)ctx;
+  struct ble_sm_io key = {
+      .action = BLE_SM_IOACT_NUMCMP,
+      .numcmp_accept = is_confirmed,
+  };
+  int rc = ble_sm_inject_io(conn_handle, &key);
+
+  PBL_LOG(LOG_LEVEL_INFO, "bt_driver_pairing_confirm: %d", rc);
+}
