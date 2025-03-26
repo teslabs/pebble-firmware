3e5466cb85252360ed14213a0ef71067219e5ca3
HalfSweet <HalfSweet@HalfSweet.cn>
2025-03-26 14:50:03 +0800

    fix: add DBG uart define

--- src/fw/board/boards/board_em_lb525.c
+++ src/fw/board/boards/board_em_lb525.c
@@ -15,4 +15,27 @@
  */
 
 #include "board/board.h"
+#include "drivers/sf32lb/uart_definitions.h"
 
+static UARTDeviceState s_dbg_uart_state;
+static UART_HandleTypeDef s_dbg_uart_handle = {.Instance = USART3,
+                                               .Init = {
+                                                   .WordLength = UART_WORDLENGTH_8B,
+                                                   .StopBits = UART_STOPBITS_1,
+                                                   .Parity = UART_PARITY_NONE,
+                                                   .HwFlowCtl = UART_HWCONTROL_NONE,
+                                                   .OverSampling = UART_OVERSAMPLING_16,
+                                               }};
+static DMA_HandleTypeDef s_dbg_uart_rx_dma_handle = {
+    .Instance = DMA1_Channel1,
+};
+static UARTDevice DBG_UART_DEVICE = {.state = &s_dbg_uart_state,
+                                     .tx_gpio = PAD_PA20,
+                                     .rx_gpio = PAD_PA27,  // TODO: Setting GPIOs to actual values
+                                     .periph = &s_dbg_uart_handle,
+                                     .rx_dma = &s_dbg_uart_rx_dma_handle,
+                                     .irq_priority = 1};
+UARTDevice *const DBG_UART = &DBG_UART_DEVICE;
+IRQ_MAP(USART3, uart_irq_handler, DBG_UART);
+
+void DMAC1_CH1_IRQHandler(void) { HAL_DMA_IRQHandler(&s_dbg_uart_rx_dma_handle); }
