04ad774a0195477141e40e4a70d2b637d7e8e69d
HalfSweet <HalfSweet@HalfSweet.cn>
2025-04-16 14:36:28 +0800

    chore: DBG_UART use USART1

--- src/fw/board/boards/board_em_lb525.c
+++ src/fw/board/boards/board_em_lb525.c
@@ -18,7 +18,7 @@
 #include "drivers/sf32lb/uart_definitions.h"
 
 static UARTDeviceState s_dbg_uart_state;
-static UART_HandleTypeDef s_dbg_uart_handle = {.Instance = USART3,
+static UART_HandleTypeDef s_dbg_uart_handle = {.Instance = USART1,
                                                .Init = {
                                                    .WordLength = UART_WORDLENGTH_8B,
                                                    .StopBits = UART_STOPBITS_1,
@@ -28,17 +28,17 @@ static UART_HandleTypeDef s_dbg_uart_handle = {.Instance = USART3,
                                                }};
 static DMA_HandleTypeDef s_dbg_uart_rx_dma_handle = {
     .Instance = DMA1_Channel1,
-    .Init.Request = DMA_REQUEST_7,
+    .Init.Request = DMA_REQUEST_4,
 };
 static UARTDevice DBG_UART_DEVICE = {.state = &s_dbg_uart_state,
-                                     .tx_gpio = PAD_PA20,
-                                     .rx_gpio = PAD_PA27,  // TODO: Setting GPIOs to actual values
+                                     .tx_gpio = PAD_PA19,
+                                     .rx_gpio = PAD_PA18,  // TODO: Setting GPIOs to actual values
                                      .periph = &s_dbg_uart_handle,
                                      .rx_dma = &s_dbg_uart_rx_dma_handle,
                                      .irq_priority = 1};
 UARTDevice *const DBG_UART = &DBG_UART_DEVICE;
 UARTDevice *const ACCESSORY_UART;  //TODO
-IRQ_MAP(USART3, uart_irq_handler, DBG_UART);
+IRQ_MAP(USART1, uart_irq_handler, DBG_UART);
 
 void DMAC1_CH1_IRQHandler(void) { HAL_DMA_IRQHandler(&s_dbg_uart_rx_dma_handle); }
 
