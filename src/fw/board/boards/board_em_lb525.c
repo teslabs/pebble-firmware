43bdd55ff525b8480f4713827e56955e3e0a495b
Xiaoming Xi <xiaomingxi@sifli.com>
2025-05-11 21:42:15 +0800

    opt: add more chip init

--- src/fw/board/boards/board_em_lb525.c
+++ src/fw/board/boards/board_em_lb525.c
@@ -162,11 +162,104 @@ void BSP_LCD_PowerUp(void)
 
 
 
+uint32_t BSP_GetOtpBase(void)
+{
+    return MPI2_MEM_BASE;
+}
+
+void HAL_PreInit(void)
+{
+    // __asm("B .");
+    /* not switch back to XT48 if other clock source has been selected already */
+    if (RCC_SYSCLK_HRC48 == HAL_RCC_HCPU_GetClockSrc(RCC_CLK_MOD_SYS))
+    {
+        // To avoid somebody cancel request.
+        HAL_HPAON_EnableXT48();
+        HAL_RCC_HCPU_ClockSelect(RCC_CLK_MOD_SYS, RCC_SYSCLK_HXT48);
+    }
+
+    HAL_RCC_HCPU_ClockSelect(RCC_CLK_MOD_HP_PERI, RCC_CLK_PERI_HXT48);
+
+    if (PM_STANDBY_BOOT != SystemPowerOnModeGet())
+    {
+        // Halt LCPU first to avoid LCPU in running state
+        HAL_HPAON_WakeCore(CORE_ID_LCPU);
+        HAL_RCC_Reset_and_Halt_LCPU(1);
+        // get system configure from EFUSE
+        BSP_System_Config();
+        HAL_HPAON_StartGTimer();
+        HAL_PMU_EnableRC32K(1);
+        HAL_PMU_LpCLockSelect(PMU_LPCLK_RC32);
+
+        HAL_PMU_EnableDLL(1);
+
+        HAL_PMU_EnableXTAL32();
+        if (HAL_PMU_LXTReady() != HAL_OK)
+            HAL_ASSERT(0);
+        // RTC/GTIME/LPTIME Using same low power clock source
+        HAL_RTC_ENABLE_LXT();
+
+        {
+            uint8_t is_enable_lxt = 1;
+            uint32_t wdt_staus = 0xFF;
+            uint32_t wdt_time = 0;
+            uint16_t wdt_clk = 32768;
+            uint8_t is_lcpu_rccal = 1;
+            HAL_LCPU_CONFIG_set(HAL_LCPU_CONFIG_XTAL_ENABLED, &is_enable_lxt, 1);
+            HAL_LCPU_CONFIG_set(HAL_LCPU_CONFIG_WDT_STATUS, &wdt_staus, 4);
+            HAL_LCPU_CONFIG_set(HAL_LCPU_CONFIG_WDT_TIME, &wdt_time, 4);
+            HAL_LCPU_CONFIG_set(HAL_LCPU_CONFIG_WDT_CLK_FEQ, &wdt_clk, 2);
+            HAL_LCPU_CONFIG_set(HAL_LCPU_CONFIG_BT_RC_CAL_IN_L, &is_lcpu_rccal, 1);
+            HAL_PMU_SetWdt((uint32_t)hwp_wdt2);   // Add reboot cause for watchdog2
+        }
+
+
+        HAL_RCC_LCPU_ClockSelect(RCC_CLK_MOD_LP_PERI, RCC_CLK_PERI_HXT48);
+
+        HAL_HPAON_CANCEL_LP_ACTIVE_REQUEST();
+    }
+
+    HAL_RCC_HCPU_ConfigHCLK(240);
+
+    // Reset sysclk used by HAL_Delay_us
+    HAL_Delay_us(0);
+    //HAL_sw_breakpoint();
+
+}
 
 void board_early_init(void) {
 
+    HAL_StatusTypeDef status;
 
+#if defined(HAL_V2D_GPU_MODULE_ENABLED)
+    HAL_RCC_ResetModule(RCC_MOD_GPU);
+#endif
 
+    HAL_PreInit();
+
+#ifdef SOC_BF0_HCPU
+    if (PM_STANDBY_BOOT != SystemPowerOnModeGet())
+    {
+        // Except Standby mode, all other boot mode need to re-calibrate RC48
+        status = HAL_RCC_CalibrateRC48();
+        HAL_ASSERT(HAL_OK == status);
+    }
+#endif /* SOC_BF0_HCPU */
+
+    HAL_RCC_Init();
+
+#ifdef SOC_BF0_HCPU
+    if (PM_STANDBY_BOOT != SystemPowerOnModeGet())
+    {
+        HAL_PMU_Init();
+    }
+#endif /* SOC_BF0_HCPU */
+
+#ifdef SOC_BF0_HCPU
+        /* init AES_ACC as normal mode */
+        __HAL_SYSCFG_CLEAR_SECURITY();
+        HAL_EFUSE_Init();
+#endif
 
 }
 
