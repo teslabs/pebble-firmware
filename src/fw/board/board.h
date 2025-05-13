26210089664ad0ea12a370b15b597481cffcc586
Gerard Marull-Paretas <gerard@teslabs.com>
2025-05-13 11:54:50 +0800

    fw/board: initial support for SF32LB
    This is really an ugly hack, same as nRF5. Pebble board layer
    needs a deep rework.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- src/fw/board/board.h
+++ src/fw/board/board.h
@@ -20,6 +20,7 @@
 #define STM32F4_COMPATIBLE
 #define STM32F7_COMPATIBLE
 #define NRF52840_COMPATIBLE
+#define SF32LB_COMPATIBLE
 #include <mcu.h>
 
 #if defined(MICRO_FAMILY_STM32F2)
@@ -30,6 +31,8 @@
 # include "board_stm32.h"
 #elif defined(MICRO_FAMILY_NRF52840)
 # include "board_nrf5.h"
+#elif defined(MICRO_FAMILY_SF32LB)
+# include "board_sf32lb.h"
 #elif !defined(SDK) && !defined(UNITTEST)
 # error "Unknown or missing MICRO_FAMILY_* define"
 #endif
